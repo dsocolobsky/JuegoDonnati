3d2d96db225f40218d6e40ca8a0f7a6fa9555add
Dylan Socolobsky <dsocolobsky@gmail.com>
2015-11-04 16:23:40 -0300

    Create basic stub for new MemoScreen class

---
+++ core/src/com/dysoco/donnati/screens/MemoScreen.java
@@ -0,0 +1,12 @@
+package com.dysoco.donnati.screens;
+
+import com.dysoco.donnati.Juego;
+import com.dysoco.donnati.Screen;
+
+public class MemoScreen extends Screen {
+
+    public MemoScreen(final Juego juego) {
+        super(juego);
+    }
+
+}
