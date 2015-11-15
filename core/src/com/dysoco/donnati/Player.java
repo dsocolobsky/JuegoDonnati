adbcbed9c18c78da2d1b369116a4d84dc738daba
Dylan Socolobsky <dsocolobsky@gmail.com>
2015-11-14 23:58:03 -0300

    Cambiar de ropa al personaje

--- core/src/com/dysoco/donnati/Player.java
+++ core/src/com/dysoco/donnati/Player.java
@@ -1,20 +1,149 @@
 package com.dysoco.donnati;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+
+import java.util.ArrayList;
 
 public class Player extends Actor {
     TextureRegion texture;
 
-    public Player(TextureRegion texture, int x, int y) {
+    private ArrayList<AtlasRegion> pelos;
+    private ArrayList<AtlasRegion> torso;
+    private ArrayList<AtlasRegion> piernas;
+    private ArrayList<AtlasRegion> zapatos;
+
+    private int pelosIndex;
+    private int torsoIndex;
+    private int piernasIndex;
+    private int zapatosIndex;
+
+    public Player(TextureRegion texture) {
         this.texture = texture;
-        setBounds(x, y, texture.getRegionWidth(), texture.getRegionHeight());
+        setBounds(0, 0, texture.getRegionWidth(), texture.getRegionHeight());
+
+        pelos = new ArrayList<AtlasRegion>();
+        torso = new ArrayList<AtlasRegion>();
+        piernas = new ArrayList<AtlasRegion>();
+        zapatos = new ArrayList<AtlasRegion>();
+
+        pelos.add(0, Assets.VACIO);
+        pelos.add(1, Assets.PELO);
+        pelos.add(2, Assets.COCINERO_GORRO);
+
+        torso.add(0, Assets.VACIO);
+        torso.add(1, Assets.BUZO);
+        torso.add(2, Assets.COCINERO_TRAJE);
+
+        piernas.add(0, Assets.VACIO);
+        piernas.add(1, Assets.PANTALONES);
+        piernas.add(2, Assets.COCINERO_PANTALONES);
+
+        zapatos.add(0, Assets.VACIO);
+        zapatos.add(1, Assets.ZAPATOS);
+        zapatos.add(2, Assets.COCINERO_ZAPATOS);
+
+        pelosIndex = 0;
+        torsoIndex = 0;
+        piernasIndex = 0;
+        zapatosIndex = 0;
+    }
+
+    public void moverPelos(int d) {
+        if(d < 0) {
+            if(pelosIndex == 0) {
+                pelosIndex = pelos.size()-1;
+            } else {
+                pelosIndex--;
+            }
+        } else {
+            if (pelosIndex == (pelos.size()-1)) {
+                pelosIndex = 0;
+            } else {
+                pelosIndex++;
+            }
+        }
+    }
+
+    public void moverTorso(int d) {
+        if(d < 0) {
+            if(torsoIndex == 0) {
+                torsoIndex = torso.size()-1;
+            } else {
+                torsoIndex--;
+            }
+        } else {
+            if (torsoIndex == (torso.size()-1)) {
+                torsoIndex = 0;
+            } else {
+                torsoIndex++;
+            }
+        }
+    }
+
+    public void moverPiernas(int d) {
+        if(d < 0) {
+            if(piernasIndex == 0) {
+                piernasIndex = piernas.size()-1;
+            } else {
+                piernasIndex--;
+            }
+        } else {
+            if (piernasIndex == (piernas.size()-1)) {
+                piernasIndex = 0;
+            } else {
+                piernasIndex++;
+            }
+        }
+    }
+
+    public void moverZapatos(int d) {
+        if(d < 0) {
+            if(zapatosIndex == 0) {
+                zapatosIndex = zapatos.size()-1;
+            } else {
+                zapatosIndex--;
+            }
+        } else {
+            if (zapatosIndex == (zapatos.size()-1)) {
+                zapatosIndex = 0;
+            } else {
+                zapatosIndex++;
+            }
+        }
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         batch.draw(texture, getX(), getY());
+
+        if(pelosIndex >= 0) {
+            if(pelosIndex == 2) {
+                batch.draw(pelos.get(pelosIndex), getX()+6, getY()+415);
+            } else {
+                batch.draw(pelos.get(pelosIndex), getX() + 27, getY() + 382);
+            }
+        }
+
+        if(piernasIndex >= 0) {
+            if(piernasIndex == 2) {
+                batch.draw(piernas.get(piernasIndex), getX()+20, getY()+32);
+            } else {
+                batch.draw(piernas.get(piernasIndex), getX() + 11, getY() + 30);
+            }
+        }
+
+        if(torsoIndex >= 0) {
+            batch.draw(torso.get(torsoIndex), getX()+11, getY()+212);
+        }
+
+        if(zapatosIndex >= 0) {
+            batch.draw(zapatos.get(zapatosIndex), getX()+11, getY()-5);
+        }
     }
 
 
--- core/src/com/dysoco/donnati/screens/EditScreen.java
+++ core/src/com/dysoco/donnati/screens/EditScreen.java
@@ -1,15 +1,15 @@
 package com.dysoco.donnati.screens;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.dysoco.donnati.*;
 
 public class EditScreen extends Screen {
-
-    Player player;
     Image fondo;
 
-    Button editButton;
+    Button volver;
 
     Button[] leftButton;
     Button[] rightButton;
@@ -20,27 +20,115 @@ public class EditScreen extends Screen {
         fondo = new Image(Assets.FONDO_TEXTURE);
         stage.addActor(fondo);
 
-        player = new Player(new TextureRegion(Assets.PLAYER_TEXTURE), 328, 5);
+        player.setPosition(328, 5);
         stage.addActor(player);
+
+        volver = new Button(new TextureRegion(Assets.BACK_BUTTON), 10, 420, 56, 56);
+
+        volver.addListener(new InputListener() {
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                juego.setScreen(new MenuScreen(juego));
+                return false;
+            }
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+            }
+        });
+        stage.addActor(volver);
 
         // BOTONES
         TextureRegion leftRegion = new TextureRegion(Assets.LEFT_BUTTON);
         TextureRegion rightRegion = new TextureRegion(Assets.RIGHT_BUTTON);
 
-        leftButton = new Button[5];
-        rightButton = new Button[5];
+        leftButton = new Button[4];
+        rightButton = new Button[4];
 
-        leftButton[0] = new Button(leftRegion, 500, 20, 35, 64); // Pies
-        leftButton[1] = new Button(leftRegion, 500, 100, 35, 64); // Pantalon
-        leftButton[2] = new Button(leftRegion, 500, 196, 35, 64); // Torso
-        leftButton[3] = new Button(leftRegion, 500, 296, 35, 64); // Cara
-        leftButton[4] = new Button(leftRegion, 500, 372, 35, 64); // Pelo
+        leftButton[0] = new Button(leftRegion, 500, 20, 35, 64);
+        leftButton[0].addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                player.moverZapatos(-1);
+                return false;
+            }
+
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            }
+        });
+
+        leftButton[1] = new Button(leftRegion, 500, 130, 35, 64);
+        leftButton[1].addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                player.moverPiernas(-1);
+                return false;
+            }
+
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            }
+        });
+
+        leftButton[2] = new Button(leftRegion, 500, 256, 35, 64);
+        leftButton[2].addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                player.moverTorso(-1);
+                return false;
+            }
+
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            }
+        });
+
+        leftButton[3] = new Button(leftRegion, 500, 372, 35, 64); // Pelo
+        leftButton[3].addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                player.moverPelos(-1);
+                return false;
+            }
+
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            }
+        });
 
         rightButton[0] = new Button(rightRegion, 236, 20, 35, 64);
-        rightButton[1] = new Button(rightRegion, 236, 100, 35, 64);
-        rightButton[2] = new Button(rightRegion, 236, 196, 35, 64);
-        rightButton[3] = new Button(rightRegion, 236, 296, 35, 64);
-        rightButton[4] = new Button(rightRegion, 236, 372, 35, 64);
+        rightButton[0].addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                player.moverZapatos(1);
+                return false;
+            }
+
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            }
+        });
+
+        rightButton[1] = new Button(rightRegion, 236, 130, 35, 64);
+        rightButton[1].addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                player.moverPiernas(1);
+                return false;
+            }
+
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            }
+        });
+
+        rightButton[2] = new Button(rightRegion, 236, 256, 35, 64);
+        rightButton[2].addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                player.moverTorso(1);
+                return false;
+            }
+
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            }
+        });
+
+        rightButton[3] = new Button(rightRegion, 236, 372, 35, 64);
+        rightButton[3].addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                player.moverPelos(1);
+                return false;
+            }
+
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+            }
+        });
 
         for(Button b : leftButton) {
             stage.addActor(b);
